13f0ff9e67398be1a0ff7ebd27817eab91ffb923
adjeielias90 <adjeielias90@gmail.com>
2020-08-24 10:04:49 +0000

    Lirary

---
+++ breast_cancer_prediction/breast_cancer_malignant_benign_prediction.py
@@ -0,0 +1,5 @@
+# import libraries
+import pandas as pd # Import Pandas for data manipulation using dataframes
+import numpy as np # Import Numpy for data statistical analysis
+import matplotlib.pyplot as plt # Import matplotlib for data visualisation
+import seaborn as sns # Statistical data visualization
