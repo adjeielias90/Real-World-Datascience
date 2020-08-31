103230d162969c718ccc6d33df79a379e99f7b12
adjeielias90 <adjeielias90@gmail.com>
2020-08-31 18:51:10 +0000

    Import test-train data

--- fashion_classifier/fashion_classifier.py
+++ fashion_classifier/fashion_classifier.py
@@ -4,3 +4,7 @@ import numpy as np # Import Numpy for data statistical analysis
 import matplotlib.pyplot as plt # Import matplotlib for data visualisation
 import seaborn as sns
 import random
+
+# dataframes creation for both training and testing datasets
+fashion_train_df = pd.read_csv('input/fashion-mnist_train.csv',sep=',')
+fashion_test_df = pd.read_csv('input/fashion-mnist_test.csv', sep = ',')
