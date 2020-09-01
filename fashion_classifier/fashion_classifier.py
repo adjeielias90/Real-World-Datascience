b04c33fabd3c82199882a624358b06e1569250b0
adjeielias90 <adjeielias90@gmail.com>
2020-09-01 15:35:23 +0000

    Prepare data

--- fashion_classifier/fashion_classifier.py
+++ fashion_classifier/fashion_classifier.py
@@ -5,6 +5,94 @@ import matplotlib.pyplot as plt # Import matplotlib for data visualisation
 import seaborn as sns
 import random
 
+# Import data
+
 # dataframes creation for both training and testing datasets
 fashion_train_df = pd.read_csv('input/fashion-mnist_train.csv',sep=',')
 fashion_test_df = pd.read_csv('input/fashion-mnist_test.csv', sep = ',')
+
+
+# Visualize the dataset
+
+# Let's view the head of the training dataset
+# 784 indicates 28x28 pixels and 1 coloumn for the label
+# After you check the tail, 60,000 training dataset are present
+fashion_train_df.head()
+
+# Let's view the last elements in the training dataset
+fashion_train_df.tail()
+
+# Let's view the head of the testing dataset
+fashion_test_df.head()
+
+# Let's view the last elements in the testing dataset
+fashion_test_df.tail()
+
+# See how data in a matrix
+fashion_train_df.shape
+
+# Create training and testing arrays
+training = np.array(fashion_train_df, dtype = 'float32')
+testing = np.array(fashion_test_df, dtype='float32')
+
+# How does it look?
+training.shape
+
+# Show me
+training
+testing
+
+# Let's view some images!
+i = random.randint(1,60000) # select any random index from 1 to 60,000
+
+# First, a revision
+# Remember the 10 classes we want to decode:
+# 0 => T-shirt/top
+# 1 => Trouser
+# 2 => Pullover
+# 3 => Dress
+# 4 => Coat
+# 5 => Sandal
+# 6 => Shirt
+# 7 => Sneaker
+# 8 => Bag
+# 9 => Ankle boot
+
+plt.imshow( training[i,1:].reshape((28,28)) ) # reshape and plot the image
+
+plt.imshow( training[i,1:].reshape((28,28)) , cmap = 'gray') # reshape and plot the image
+
+
+label = training[i,0]
+label
+
+# Let's view more images in a grid format
+# Define the dimensions of the plot grid
+W_grid = 15
+L_grid = 15
+
+# fig, axes = plt.subplots(L_grid, W_grid)
+# Subplots return the figure object and axes object
+# Then, we can then use the axes objects to plot specific figures at various locations
+
+fig, axes = plt.subplots(L_grid, W_grid, figsize = (17,17))
+
+# flaten the 15 x 15 matrix into 225 array
+axes = axes.ravel()
+
+# get the length of the training dataset
+n_training = len(training)
+
+# Select a random number from 0 to n_training
+for i in np.arange(0, W_grid * L_grid): # create evenly spaces variables
+
+    # Select a random number
+    index = np.random.randint(0, n_training)
+    # read and display an image with the selected index
+    axes[i].imshow( training[index,1:].reshape((28,28)) )
+    axes[i].set_title(training[index,0], fontsize = 8)
+    axes[i].axis('off')
+
+plt.subplots_adjust(hspace=0.4)
+
+
