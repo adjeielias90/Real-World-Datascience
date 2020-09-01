7fcee2df58407070ea2f4310b5231efcc3cfd603
adjeielias90 <adjeielias90@gmail.com>
2020-09-01 15:51:39 +0000

    Add and evaluate fashion classifier

--- fashion_classifier/fashion_classifier.py
+++ fashion_classifier/fashion_classifier.py
@@ -6,10 +6,13 @@ import seaborn as sns
 import random
 from sklearn.model_selection import train_test_split
 import keras
+# NN library madke our life much easier
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 from keras.optimizers import Adam
 from keras.callbacks import TensorBoard
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
 # Import data
 
@@ -94,7 +97,8 @@ for i in np.arange(0, W_grid * L_grid): # create evenly spaces variables
 
     # Select a random number
     index = np.random.randint(0, n_training)
-    # read and display an image with the selected index
+
+    # Read and display an image with the selected index
     axes[i].imshow( training[index,1:].reshape((28,28)) )
     axes[i].set_title(training[index,0], fontsize = 8)
     axes[i].axis('off')
@@ -114,7 +118,7 @@ X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train, te
 X_train.shape
 y_train.shape
 
-# * unpack the tuple
+# * Unpack the tuple
 X_train = X_train.reshape(X_train.shape[0], *(28, 28, 1))
 X_test = X_test.reshape(X_test.shape[0], *(28, 28, 1))
 X_validate = X_validate.reshape(X_validate.shape[0], *(28, 28, 1))
@@ -124,3 +128,73 @@ X_train.shape
 X_test.shape
 
 X_validate.shape
+
+# y_train = keras.utils.to_categorical(y_train, 10)
+# y_test = keras.utils.to_categorical(y_test, 10)
+
+# Initialize convolutional neural network
+cnn_model = Sequential()
+
+# Try 32 fliters first then 64
+cnn_model.add(Conv2D(64,3, 3, input_shape = (28,28,1), activation='relu'))
+cnn_model.add(MaxPooling2D(pool_size = (2, 2)))
+
+cnn_model.add(Dropout(0.25))
+
+# cnn_model.add(Conv2D(32,3, 3, activation='relu'))
+# cnn_model.add(MaxPooling2D(pool_size = (2, 2)))
+cnn_model.add(Flatten())
+# Add layer with relu activation function
+cnn_model.add(Dense(output_dim = 32, activation = 'relu'))
+# Another layer with sigmoid activation function
+cnn_model.add(Dense(output_dim = 10, activation = 'sigmoid'))
+
+# Compile CNN with an SCC loss function
+# Set metrics to accuracy
+# Use Adam optimizer with a learning rate of 0.001
+cnn_model.compile(loss ='sparse_categorical_crossentropy', optimizer=Adam(lr=0.001),metrics =['accuracy'])
+
+# Specify number of epochs to our model's traning
+epochs = 50
+
+
+history = cnn_model.fit(X_train,
+                        y_train,
+                        batch_size = 512,
+                        nb_epoch = epochs,
+                        verbose = 1,
+                        validation_data = (X_validate, y_validate))
+
+
+# Finally let's evaluate our work that's going to earn us millions
+evaluation = cnn_model.evaluate(X_test, y_test)
+print('Test Accuracy : {:.3f}'.format(evaluation[1]))
+
+# Get the predictions for the test data
+predicted_classes = cnn_model.predict_classes(X_test)
+
+
+L = 5
+W = 5
+fig, axes = plt.subplots(L, W, figsize = (12,12))
+axes = axes.ravel() #
+
+for i in np.arange(0, L * W):
+    axes[i].imshow(X_test[i].reshape(28,28))
+    axes[i].set_title("Prediction Class = {:0.1f}\n True Class = {:0.1f}".format(predicted_classes[i], y_test[i]))
+    axes[i].axis('off')
+
+plt.subplots_adjust(wspace=0.5)
+
+
+# Initialize confusion matrix, annotate result with heatmap plot
+# Sum the diagonal element to get the total true correct values
+cm = confusion_matrix(y_test, predicted_classes)
+plt.figure(figsize = (14,10))
+sns.heatmap(cm, annot=True)
+
+# Moment of truth lol
+num_classes = 10
+target_names = ["Class {}".format(i) for i in range(num_classes)]
+
+print(classification_report(y_test, predicted_classes, target_names = target_names))
