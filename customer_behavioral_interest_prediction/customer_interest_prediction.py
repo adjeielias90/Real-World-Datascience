0f7e798695278d2a68beb29998efa73f29bcb6be
adjeielias90 <adjeielias90@gmail.com>
2020-09-16 08:28:05 +0000

    Add histogram, clean data

--- customer_behavioral_interest_prediction/customer_interest_prediction.py
+++ customer_behavioral_interest_prediction/customer_interest_prediction.py
@@ -13,3 +13,52 @@ dataset.head(10)
 
 # Distribution of numerical variables
 dataset.describe()
+
+# First set of Feature cleaning
+dataset["hour"] = dataset.hour.str.slice(1, 3).astype(int)
+
+### Plotting
+dataset2 = dataset.copy().drop(columns = ['user', 'screen_list', 'enrolled_date',
+                                           'first_open', 'enrolled'])
+dataset2.head()
+
+## Histograms
+plt.suptitle('Histograms of Numerical Columns', fontsize=20)
+for i in range(1, dataset2.shape[1] + 1):
+    plt.subplot(3, 3, i)
+    f = plt.gca()
+#    f.axes.get_yaxis().set_visible(False)
+    f.set_title(dataset2.columns.values[i - 1])
+
+    vals = np.size(dataset2.iloc[:, i - 1].unique())
+
+    plt.hist(dataset2.iloc[:, i - 1], bins=vals, color='#3F5D7D')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+#plt.savefig('app_data_hist.jpg')
+
+## Correlation with Response Variable
+dataset2.corrwith(dataset.enrolled).plot.bar(figsize=(20,10),
+                  title = 'Correlation with Reposnse variable',
+                  fontsize = 15, rot = 45,
+                  grid = True)
+
+## Correlation Matrix
+sn.set(style="white", font_scale=2)
+
+# Compute the correlation matrix
+corr = dataset2.corr()
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(18, 15))
+f.suptitle("Correlation Matrix", fontsize = 40)
+
+# Generate a custom diverging colormap
+cmap = sn.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sn.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
