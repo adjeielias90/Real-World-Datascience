200fa746cd407794638dbd38028041a2506c56ab
adjeielias90 <adjeielias90@gmail.com>
2020-09-16 10:52:01 +0000

    Add dataset. Handle imports

---
+++ customer_churn_based_on_financial_behavior/reduce_customer_churn_rate.py
@@ -0,0 +1,8 @@
+# Importing Libraries
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sn
+
+dataset = pd.read_csv('churn_data.csv') # Users who were 60 days enrolled, churn in the next 30
