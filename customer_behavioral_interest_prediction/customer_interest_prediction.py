ab9a57b501fd22da9d196b2fa7fb57e56d5efbd2
adjeielias90 <adjeielias90@gmail.com>
2020-09-16 09:11:40 +0000

    Feature engineering clean data prepare data for model

--- customer_behavioral_interest_prediction/customer_interest_prediction.py
+++ customer_behavioral_interest_prediction/customer_interest_prediction.py
@@ -37,6 +37,7 @@ plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 #plt.savefig('app_data_hist.jpg')
 
 ## Correlation with Response Variable
+# return correlation of all  data fields, plot result
 dataset2.corrwith(dataset.enrolled).plot.bar(figsize=(20,10),
                   title = 'Correlation with Reposnse variable',
                   fontsize = 15, rot = 45,
@@ -64,3 +65,80 @@ sn.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
             square=True, linewidths=.5, cbar_kws={"shrink": .5})
 
 #### Feature Engineering ####
+
+# Formatting Date Columns
+dataset.dtypes
+dataset["first_open"] = [parser.parse(row_date) for row_date in dataset["first_open"]]
+dataset["enrolled_date"] = [parser.parse(row_date) if isinstance(row_date, str) else row_date for row_date in dataset["enrolled_date"]]
+dataset.dtypes
+
+# Selecting Time For Response
+dataset["difference"] = (dataset.enrolled_date-dataset.first_open).astype('timedelta64[h]')
+response_hist = plt.hist(dataset["difference"].dropna(), color='#3F5D7D')
+plt.title('Distribution of Time-Since-Screen-Reached')
+plt.show()
+
+plt.hist(dataset["difference"].dropna(), color='#3F5D7D', range = [0, 100])
+plt.title('Distribution of Time-Since-Screen-Reached')
+plt.show()
+
+dataset.loc[dataset.difference > 48, 'enrolled'] = 0
+dataset = dataset.drop(columns=['enrolled_date', 'difference', 'first_open'])
+
+## Formatting the screen_list Field
+
+# Load Top Screens
+top_screens = pd.read_csv('top_screens.csv').top_screens.values
+top_screens
+
+# Mapping Screens to Fields
+dataset["screen_list"] = dataset.screen_list.astype(str) + ','
+
+for sc in top_screens:
+    dataset[sc] = dataset.screen_list.str.contains(sc).astype(int)
+    dataset['screen_list'] = dataset.screen_list.str.replace(sc+",", "")
+
+dataset['Other'] = dataset.screen_list.str.count(",")
+dataset = dataset.drop(columns=['screen_list'])
+
+# Funnels
+savings_screens = ["Saving1",
+                    "Saving2",
+                    "Saving2Amount",
+                    "Saving4",
+                    "Saving5",
+                    "Saving6",
+                    "Saving7",
+                    "Saving8",
+                    "Saving9",
+                    "Saving10"]
+dataset["SavingCount"] = dataset[savings_screens].sum(axis=1)
+dataset = dataset.drop(columns=savings_screens)
+
+cm_screens = ["Credit1",
+               "Credit2",
+               "Credit3",
+               "Credit3Container",
+               "Credit3Dashboard"]
+dataset["CMCount"] = dataset[cm_screens].sum(axis=1)
+dataset = dataset.drop(columns=cm_screens)
+
+cc_screens = ["CC1",
+                "CC1Category",
+                "CC3"]
+dataset["CCCount"] = dataset[cc_screens].sum(axis=1)
+dataset = dataset.drop(columns=cc_screens)
+
+loan_screens = ["Loan",
+               "Loan2",
+               "Loan3",
+               "Loan4"]
+dataset["LoansCount"] = dataset[loan_screens].sum(axis=1)
+dataset = dataset.drop(columns=loan_screens)
+
+#### Saving Results ####
+dataset.head()
+dataset.describe()
+dataset.columns
+
+dataset.to_csv('new_appdata10.csv', index = False)
