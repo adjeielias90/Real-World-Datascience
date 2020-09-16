5dcee0fceed0f7380f5a751232eec7b914ed8786
adjeielias90 <adjeielias90@gmail.com>
2020-09-16 12:10:39 +0000

    Pie charts

--- customer_churn_based_on_financial_behavior/reduce_customer_churn_rate.py
+++ customer_churn_based_on_financial_behavior/reduce_customer_churn_rate.py
@@ -23,7 +23,7 @@ dataset.isna().any()
 dataset.isna().sum()
 dataset = dataset.drop(columns = ['credit_score', 'rewards_earned'])
 
-## Histograms
+# Histograms
 dataset2 = dataset.drop(columns = ['user', 'churn'])
 fig = plt.figure(figsize=(15, 12))
 plt.suptitle('Histograms of Numerical Columns', fontsize=20)
@@ -37,3 +37,25 @@ for i in range(1, dataset2.shape[1] + 1):
 
     plt.hist(dataset2.iloc[:, i - 1], bins=vals, color='#3F5D7D')
 plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+
+# Add Pie Plots
+dataset2 = dataset[['housing', 'is_referred', 'app_downloaded',
+                    'web_user', 'app_web_user', 'ios_user',
+                    'android_user', 'registered_phones', 'payment_type',
+                    'waiting_4_loan', 'cancelled_loan',
+                    'received_loan', 'rejected_loan', 'zodiac_sign',
+                    'left_for_two_month_plus', 'left_for_one_month', 'is_referred']]
+fig = plt.figure(figsize=(15, 12))
+plt.suptitle('Pie Chart Distributions', fontsize=20)
+for i in range(1, dataset2.shape[1] + 1):
+    plt.subplot(6, 3, i)
+    f = plt.gca()
+    f.axes.get_yaxis().set_visible(False)
+    f.set_title(dataset2.columns.values[i - 1])
+
+    values = dataset2.iloc[:, i - 1].value_counts(normalize = True).values
+    index = dataset2.iloc[:, i - 1].value_counts(normalize = True).index
+    plt.pie(values, labels = index, autopct='%1.1f%%')
+    plt.axis('equal')
+fig.tight_layout(rect=[0, 0.03, 1, 0.95])
