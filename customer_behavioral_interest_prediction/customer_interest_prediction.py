9a74b9bf354e22bf72b1a30d3b41a437369f178c
adjeielias90 <adjeielias90@gmail.com>
2020-09-16 09:50:52 +0000

    Test model

--- customer_behavioral_interest_prediction/customer_interest_prediction.py
+++ customer_behavioral_interest_prediction/customer_interest_prediction.py
@@ -8,6 +8,9 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 import time
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
 
 dataset = pd.read_csv('appdata10.csv')
 
@@ -173,23 +176,69 @@ X_train, X_test, y_train, y_test = train_test_split(dataset, response,
                                                     random_state = 0)
 
 ## Balancing the Training Set
-import random
-y_train.value_counts()
+# import random
+# y_train.value_counts()
 
-pos_index = y_train[y_train.values == 1].index
-neg_index = y_train[y_train.values == 0].index
+# pos_index = y_train[y_train.values == 1].index
+# neg_index = y_train[y_train.values == 0].index
 
-if len(pos_index) > len(neg_index):
-   higher = pos_index
-   lower = neg_index
-else:
-   higher = neg_index
-   lower = pos_index
+# if len(pos_index) > len(neg_index):
+#    higher = pos_index
+#    lower = neg_index
+# else:
+#    higher = neg_index
+#    lower = pos_index
 
-random.seed(0)
-higher = np.random.choice(higher, size=len(lower))
-lower = np.asarray(lower)
-new_indexes = np.concatenate((lower, higher))
+# random.seed(0)
+# higher = np.random.choice(higher, size=len(lower))
+# lower = np.asarray(lower)
+# new_indexes = np.concatenate((lower, higher))
 
-X_train = X_train.loc[new_indexes,]
-y_train = y_train[new_indexes]
+# X_train = X_train.loc[new_indexes,]
+# y_train = y_train[new_indexes]
+
+
+# Removing Identifiers
+train_identity = X_train['user']
+X_train = X_train.drop(columns = ['user'])
+test_identity = X_test['user']
+X_test = X_test.drop(columns = ['user'])
+
+# Feature Scaling
+sc_X = StandardScaler()
+X_train2 = pd.DataFrame(sc_X.fit_transform(X_train))
+X_test2 = pd.DataFrame(sc_X.transform(X_test))
+X_train2.columns = X_train.columns.values
+X_test2.columns = X_test.columns.values
+X_train2.index = X_train.index.values
+X_test2.index = X_test.index.values
+X_train = X_train2
+X_test = X_test2
+
+
+
+
+#### Model Building ####
+
+# Fitting Model to the Training Set
+classifier = LogisticRegression(random_state = 0, penalty = 'l1')
+classifier.fit(X_train, y_train)
+
+
+# Predicting Test Set
+
+y_pred = classifier.predict(X_test)
+
+# Evaluating Results
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+cm = confusion_matrix(y_test, y_pred)
+accuracy_score(y_test, y_pred)
+precision_score(y_test, y_pred) # tp / (tp + fp)
+recall_score(y_test, y_pred) # tp / (tp + fn)
+f1_score(y_test, y_pred)
+
+df_cm = pd.DataFrame(cm, index = (0, 1), columns = (0, 1))
+plt.figure(figsize = (10,7))
+sn.set(font_scale=1.4)
+sn.heatmap(df_cm, annot=True, fmt='g')
+print("Test Data Accuracy: %0.4f" % accuracy_score(y_test, y_pred))
