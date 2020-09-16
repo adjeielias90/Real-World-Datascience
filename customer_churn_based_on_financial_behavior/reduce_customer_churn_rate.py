56fc2766bc9e1999c53bfa9a07ed6eb479108288
adjeielias90 <adjeielias90@gmail.com>
2020-09-16 12:09:34 +0000

    Histograms

--- customer_churn_based_on_financial_behavior/reduce_customer_churn_rate.py
+++ customer_churn_based_on_financial_behavior/reduce_customer_churn_rate.py
@@ -22,3 +22,18 @@ dataset = dataset[dataset.credit_score >= 300]
 dataset.isna().any()
 dataset.isna().sum()
 dataset = dataset.drop(columns = ['credit_score', 'rewards_earned'])
+
+## Histograms
+dataset2 = dataset.drop(columns = ['user', 'churn'])
+fig = plt.figure(figsize=(15, 12))
+plt.suptitle('Histograms of Numerical Columns', fontsize=20)
+for i in range(1, dataset2.shape[1] + 1):
+    plt.subplot(6, 5, i)
+    f = plt.gca()
+    f.axes.get_yaxis().set_visible(False)
+    f.set_title(dataset2.columns.values[i - 1])
+
+    vals = np.size(dataset2.iloc[:, i - 1].unique())
+
+    plt.hist(dataset2.iloc[:, i - 1], bins=vals, color='#3F5D7D')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
