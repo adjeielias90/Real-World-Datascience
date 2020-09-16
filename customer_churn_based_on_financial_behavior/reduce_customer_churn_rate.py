86897c539ea89928af0cc8af1eeb088d25a185c5
adjeielias90 <adjeielias90@gmail.com>
2020-09-16 16:03:22 +0000

    Separate model from preprocessing code

--- customer_churn_based_on_financial_behavior/reduce_customer_churn_rate.py
+++ customer_churn_based_on_financial_behavior/reduce_customer_churn_rate.py
@@ -102,8 +102,7 @@ sn.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
 # Removing Correlated Fields
 dataset = dataset.drop(columns = ['app_web_user'])
 
-## Note: Although there are somewhat correlated fields, they are not colinear
-## These feature are not functions of each other, so they won't break the model
-## But these feature won't help much either. Feature Selection should remove them.
-
+# Note: Although there are somewhat correlated fields, they are not colinear
+# These feature are not functions of each other, so they won't break the model
+# But these feature won't help much either. Feature Selection should remove them.
 dataset.to_csv('new_churn_data.csv', index = False)
