6b8e0676cbc1653c374b48506d8698a41221e973
adjeielias90 <adjeielias90@gmail.com>
2020-09-16 12:20:58 +0000

    Generate heatmap

--- customer_churn_based_on_financial_behavior/reduce_customer_churn_rate.py
+++ customer_churn_based_on_financial_behavior/reduce_customer_churn_rate.py
@@ -92,3 +92,13 @@ mask[np.triu_indices_from(mask)] = True
 # Set up the matplotlib figure
 f, ax = plt.subplots(figsize=(18, 15))
 
+# Generate a custom diverging colormap
+cmap = sn.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sn.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+# Removing Correlated Fields
+dataset = dataset.drop(columns = ['app_web_user'])
+
