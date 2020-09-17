537eca82ee14c720345245e0bbf6096608d5ec4e
adjeielias90 <adjeielias90@gmail.com>
2020-09-17 11:31:24 +0000

    Inspect

--- customer_churn_based_on_financial_behavior/reduce_customer_churn_rate_model.py
+++ customer_churn_based_on_financial_behavior/reduce_customer_churn_rate_model.py
@@ -5,9 +5,16 @@ import numpy as np
 import random
 import seaborn as sn
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 dataset = pd.read_csv('new_churn_data.csv')
 
 # Data Preparation
 user_identifier = dataset['user']
 dataset = dataset.drop(columns = ['user'])
+
+# One-Hot Encoding
+dataset.housing.value_counts()
+dataset.groupby('housing')['churn'].nunique().reset_index()
+dataset = pd.get_dummies(dataset)
+dataset.columns
