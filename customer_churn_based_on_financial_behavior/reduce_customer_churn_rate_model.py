4e2099e9b5ac9ec98dab8c9f6092db49f03d9803
adjeielias90 <adjeielias90@gmail.com>
2020-09-17 11:29:56 +0000

    Prep dataset

--- customer_churn_based_on_financial_behavior/reduce_customer_churn_rate_model.py
+++ customer_churn_based_on_financial_behavior/reduce_customer_churn_rate_model.py
@@ -7,3 +7,7 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('new_churn_data.csv')
+
+# Data Preparation
+user_identifier = dataset['user']
+dataset = dataset.drop(columns = ['user'])
