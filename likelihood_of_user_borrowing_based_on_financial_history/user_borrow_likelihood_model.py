9d0afb71e7403f06f41daaa8b1a6a8958c9ea027
adjeielias90 <adjeielias90@gmail.com>
2020-09-26 16:26:10 +0000

    Remove redundant/unused features

--- likelihood_of_user_borrowing_based_on_financial_history/user_borrow_likelihood_model.py
+++ likelihood_of_user_borrowing_based_on_financial_history/user_borrow_likelihood_model.py
@@ -11,3 +11,9 @@ random.seed(100)
 
 # Data Preprocessing
 dataset = pd.read_csv('financial_data.csv')
+
+# Feature Engineering
+dataset = dataset.drop(columns = ['months_employed'])
+dataset['personal_account_months'] = (dataset.personal_account_m + (dataset.personal_account_y * 12))
+dataset[['personal_account_m', 'personal_account_y', 'personal_account_months']].head()
+dataset = dataset.drop(columns = ['personal_account_m', 'personal_account_y'])
