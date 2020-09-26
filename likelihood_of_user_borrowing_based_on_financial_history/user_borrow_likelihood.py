077b3e091b2f0e30a183c9bf7b5f905c3e541952
adjeielias90 <adjeielias90@gmail.com>
2020-09-26 16:19:11 +0000

    Correlation matrix, Determine relationships

--- likelihood_of_user_borrowing_based_on_financial_history/user_borrow_likelihood.py
+++ likelihood_of_user_borrowing_based_on_financial_history/user_borrow_likelihood.py
@@ -54,3 +54,27 @@ plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 dataset2.corrwith(dataset.e_signed).plot.bar(
         figsize = (20, 10), title = "Correlation with E Signed", fontsize = 15,
         rot = 45, grid = True)
+
+
+# Correlation Matrix to tell us the relationships between our columns(attributes)
+
+## Correlation Matrix
+sn.set(style="white")
+
+# Compute the correlation matrix
+# This time we will use corr(), a more robust alternative to the corrwith() function
+corr = dataset2.corr()
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(18, 15))
+
+# Generate a custom diverging colormap
+cmap = sn.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sn.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
