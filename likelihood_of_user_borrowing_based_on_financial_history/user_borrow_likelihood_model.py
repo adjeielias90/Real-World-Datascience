3737210b52872b7fe0fc03ab684a44bed0937ae7
adjeielias90 <adjeielias90@gmail.com>
2020-09-26 16:26:42 +0000

    Feature engineering

--- likelihood_of_user_borrowing_based_on_financial_history/user_borrow_likelihood_model.py
+++ likelihood_of_user_borrowing_based_on_financial_history/user_borrow_likelihood_model.py
@@ -13,6 +13,7 @@ random.seed(100)
 dataset = pd.read_csv('financial_data.csv')
 
 # Feature Engineering
+
 dataset = dataset.drop(columns = ['months_employed'])
 dataset['personal_account_months'] = (dataset.personal_account_m + (dataset.personal_account_y * 12))
 dataset[['personal_account_m', 'personal_account_y', 'personal_account_months']].head()
