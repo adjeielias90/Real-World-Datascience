fce203d5fcfad2c2d6f8da7462ce66c22db4e47e
adjeielias90 <adjeielias90@gmail.com>
2020-09-26 16:29:56 +0000

    Better sanitize data

--- likelihood_of_user_borrowing_based_on_financial_history/user_borrow_likelihood_model.py
+++ likelihood_of_user_borrowing_based_on_financial_history/user_borrow_likelihood_model.py
@@ -15,6 +15,7 @@ dataset = pd.read_csv('financial_data.csv')
 # Feature Engineering
 
 dataset = dataset.drop(columns = ['months_employed'])
+# Better sanitize instances of period into months/years
 dataset['personal_account_months'] = (dataset.personal_account_m + (dataset.personal_account_y * 12))
 dataset[['personal_account_m', 'personal_account_y', 'personal_account_months']].head()
 dataset = dataset.drop(columns = ['personal_account_m', 'personal_account_y'])
@@ -24,3 +25,10 @@ dataset = dataset.drop(columns = ['personal_account_m', 'personal_account_y'])
 dataset = pd.get_dummies(dataset)
 dataset.columns
 dataset = dataset.drop(columns = ['pay_schedule_semi-monthly'])
+
+
+# Removing extra columns
+
+response = dataset["e_signed"]
+users = dataset['entry_id']
+dataset = dataset.drop(columns = ["e_signed", "entry_id"])
