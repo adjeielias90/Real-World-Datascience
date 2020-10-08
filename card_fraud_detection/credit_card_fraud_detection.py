0d4fd8c574e7db3025e70ad4d286160ca6ea55fb
adjeielias90 <adjeielias90@gmail.com>
2020-10-08 14:12:36 +0000

    train test sets

--- card_fraud_detection/credit_card_fraud_detection.py
+++ card_fraud_detection/credit_card_fraud_detection.py
@@ -25,3 +25,19 @@ data.head()
 
 data = data.drop(['Time'],axis=1)
 data.head()
+
+X = data.iloc[:, data.columns != 'Class']
+y = data.iloc[:, data.columns == 'Class']
+
+y.head()
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3, random_state=0)
+
+X_train.shape
+X_test.shape
+
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
