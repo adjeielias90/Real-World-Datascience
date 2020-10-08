ab58bc28fa5180ea9dde8a50a38c77e72da17ade
adjeielias90 <adjeielias90@gmail.com>
2020-10-08 15:47:12 +0000

    Add confusion matrix

--- card_fraud_detection/credit_card_fraud_detection.py
+++ card_fraud_detection/credit_card_fraud_detection.py
@@ -9,6 +9,13 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 
+
+import matplotlib.pyplot as plt
+import itertools
+
+from sklearn import svm, datasets
+from sklearn.metrics import confusion_matrix
+
 np.random.seed(2)
 
 # small EDA, visualize data
@@ -60,8 +67,62 @@ model = Sequential([
 
 
 model.summary()
+
+# Train our model
 model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 model.fit(X_train,y_train,batch_size=15,epochs=5)
 
 score = model.evaluate(X_test, y_test)
 print(score)
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+
+y_pred = model.predict(X_test)
+y_test = pd.DataFrame(y_test)
+
+cnf_matrix = confusion_matrix(y_test, y_pred.round())
+
+print(cnf_matrix)
+plot_confusion_matrix(cnf_matrix, classes=[0,1])
+
+plt.show()
+
+y_pred = model.predict(X)
+y_expected = pd.DataFrame(y)
+cnf_matrix = confusion_matrix(y_expected, y_pred.round())
+plot_confusion_matrix(cnf_matrix,classes=[0,1])
+plt.show()
