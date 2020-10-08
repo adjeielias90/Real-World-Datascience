82d1acdab1fd72d3a0da0c5ca71c219768ae8e58
adjeielias90 <adjeielias90@gmail.com>
2020-10-08 13:52:15 +0000

    Pre-process dataset

--- card_fraud_detection/credit_card_fraud_detection.py
+++ card_fraud_detection/credit_card_fraud_detection.py
@@ -2,5 +2,26 @@
 import pandas as pd
 import numpy as np
 import keras
+from sklearn.preprocessing import StandardScaler
 
 np.random.seed(2)
+
+# small EDA, visualize data
+
+data = pd.read_csv('creditcard.csv')
+data.head()
+
+# Pre-processing
+
+# let's quickly reshape our amount column
+data['normalizedAmount'] = StandardScaler().fit_transform(data['Amount'].values.reshape(-1,1))
+
+# remove what we don't need
+data = data.drop(['Amount'],axis=1)
+
+# see what we have
+data.head()
+
+
+data = data.drop(['Time'],axis=1)
+data.head()
